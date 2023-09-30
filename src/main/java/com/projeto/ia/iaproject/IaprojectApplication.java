e30a2e9a5a19bf91bb9895a206f624663908cbde
Pedro <pedro.link25@gmail.com>
2023-09-30 17:18:54 -0300

    INITIAL COMMIT Commit inicial

---
+++ src/main/java/com/projeto/ia/iaproject/IaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ia.iaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/ia/iaproject/IaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ia.iaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
